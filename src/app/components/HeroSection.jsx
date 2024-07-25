e2bab76728260a46318dd4f7180db0c7aeaa33f2
Mustafa Mohmed <randka249@gmail.com>
2024-07-25 16:17:33 +0300

    add new cv

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -46,7 +46,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="https://drive.google.com/file/d/1_U9X-CJTfRdZ9-xFKUJWdi0wJOOebS9i/view?usp=drive_link"
+              href="https://drive.google.com/file/d/1l97TE-EKriD5zC6ArxppihGO1N7K_Add/view?usp=drive_link"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
